4f24598e4131f215f1be71024c1a7801e90b5ef1
Antonio Pierro <antonio.pierro@gmail.com>
2014-08-29 12:43:13 +0200

    added test for the macroarea list

--- src/Innovare/CoreBundle/Tests/Controller/MacroareaControllerTest.php
+++ src/Innovare/CoreBundle/Tests/Controller/MacroareaControllerTest.php
@@ -4,13 +4,21 @@ namespace Innovare\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/*
+ * Class MacroareaControllerTest
+ */
 class MacroareaControllerTest extends WebTestCase
 {
+	/*
+	 * Tests Macroareas list
+	 */
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
     }
 
 }
