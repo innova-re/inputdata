10bb2b32dd9433808acd7087612dab3c36face29
Antonio Pierro <antonio.pierro@gmail.com>
2014-08-29 11:39:16 +0200

    simplified the db schema

---
+++ app/DoctrineMigrations/Version20140829053335.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140829053335 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE macroarea CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE nome nome VARCHAR(250) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE macroarea CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL, CHANGE nome nome VARCHAR(250) DEFAULT \'\' NOT NULL');
+    }
+}
