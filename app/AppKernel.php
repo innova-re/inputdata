b945b95e323a1eed0de677f4d57e1cd467c827a7
Antonio Pierro <antonio.pierro@gmail.com>
2014-09-02 14:54:22 +0200

    Added bootstrap-braincrafted bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Innovare\CoreBundle\InnovareCoreBundle(),
             new Innovare\ModelBundle\InnovareModelBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
